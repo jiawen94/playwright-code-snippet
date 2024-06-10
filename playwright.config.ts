102dc0ba450cdf26e4378db5473ab6e8d33d11a9
jiawen <zhaojiawen94@outlook.com>
2024-06-10 21:04:08 +0800

    monocart issue 122

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,161 @@ export default defineConfig({
 
     [
       ['list'],
-      ['html'],
+      //['html'],
+
+
+      ['monocart-reporter', {
+
+        // the report name
+        name: `${process.env.MODEL}_${process.env.MODULE}_${process.env.ENGINE_NAME} `,
+
+        // the output file path (relative process.cwd)
+        outputFile: './test-results/report.html',
+
+        mermaid: {
+          // mermaid script url, using mermaid CDN: https://www.jsdelivr.com/package/npm/mermaid
+          scriptSrc: 'https://cdn.jsdelivr.net/npm/mermaid@latest/dist/mermaid.min.js',
+          // mermaid config: https://mermaid.js.org/config/schema-docs/config.html
+          config: {
+            startOnLoad: false
+          }
+        },
+
+        // attachment path handler
+        attachmentPath: null,
+        // attachmentPath: (currentPath, extras) => `https://another-path/${currentPath}`,
+
+        traceViewerUrl: 'https://trace.playwright.dev/?trace={traceUrl}',
+
+        // logging levels: off, error, info, debug
+        logging: 'info',
+
+        // timezone offset in minutes, GMT+0800 = -480
+        timezoneOffset: 0,
+
+        // global coverage settings for addCoverageReport API
+        coverage: null,
+        // coverage: {
+        //     entryFilter: (entry) => true,
+        //     sourceFilter: (sourcePath) => sourcePath.search(/src\/.+/) !== -1,
+        // },
+
+        // trend data handler
+        trend: null,
+        // trend: () => './test-results/report.json',
+
+        // custom tags style
+        tags: {
+          smoke: {
+            style: {
+              background: '#6F9913'
+            },
+            description: 'smoke test.'
+          },
+          init: {
+            style: 'background:#178F43;',
+            description: '初始化环境'
+          },
+          critical: {
+            style: 'background:orange;',
+            description: '关键功能'
+          },
+          enable: {
+            style: 'background:green;',
+            description: '开启'
+          },
+          login: {
+            style: 'background:blue;',
+            description: 'login the system.'
+          },
+          setup: {
+            style: 'background:green;',
+            description: '预备环境'
+          },
+          spark: {
+            style: 'background:#FF3030;',
+            description: 'Spark统一认证平台'
+          },
+          ccmgr: {
+            style: 'background:#42D1f5;',
+            description: '云管平台'
+          },
+          ckmgr: {
+            style: 'background:#9ACD32;',
+            description: '密管平台'
+          },
+          admin: {
+            style: 'background:#F5D142;',
+            description: '管理员'
+          },
+          tenant: {
+            style: 'background:#D742F5;',
+            description: '租户'
+          },
+        },
+
+        // columns data handler
+        //columns: null,
+        // columns: (defaultColumns) => {},
+
+
+        columns: (defaultColumns) => {
+          // insert custom column(s) before a default column
+          const index = defaultColumns.findIndex((column) => column.id === 'duration');
+
+          defaultColumns.splice(index, 0, {
+            // define the column in reporter
+            id: 'owner',
+            name: 'Owner',
+            align: 'center',
+            searchable: true,
+            styleMap: {
+              'font-weight': 'normal'
+            }
+          },
+
+
+            {
+              // another column for mantisBT link
+              id: 'mantis',
+              name: 'MantisBT',
+              width: 75,
+              searchable: true,
+              styleMap: 'font-weight:normal;',
+              /**
+               * 
+               * @param v Mantis ID
+               * @param rowItem 
+               * @param columnItem   
+               * @returns a link
+               */
+              formatter: (v, rowItem, columnItem) => {
+                const key = rowItem[columnItem.id];
+                /** 目前MantisBT的BaseURL用变量的形式会导致报告出现空白的BUG */
+                return `<a href="http://192.168.111.111/bt/view.php?id=${key}" target="_blank">${v}</a>`;
+              }
+            },
+          )
+
+          defaultColumns.push({
+            id: 'description',
+            name: 'Description',
+            width: 110,
+            markdown: true,
+            searchable: true
+          })
+
+        },
+
+        // rows data handler (suite, case and step)
+        visitor: null,
+        // visitor: (data, metadata, collect) => {},
+        customFieldsInComments: true,
+        // onEnd hook
+        onEnd: null
+        // onEnd: async (reportData, capability) => {}
+
+      }]
 
      
     ],
--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,30 +1,15 @@
 import { test, expect } from '@playwright/test';
 
+    /**
+     * @owner jiawen
+     * @description
+     * test test0
+     */
+
 test('test0', async ({ page }) => {
   await page.goto('https://www.baidu.com/');
   await page.locator('#kw').click();
   await page.locator('#kw').fill('playwright');
   await page.getByRole('button', { name: '百度一下' }).click();
 
-});
-
-test('test1', async ({ page }) => {
-  await page.goto('https://www.baidu.com/');
-  await page.locator('#kw').click();
-
-  await page.waitForTimeout(5_000)
-  await page.locator('#kw').fill('hehehe');
-  await page.getByRole('button', { name: '百度一下' }).click();
-
-
-});
-
-test('test2', async ({ page }) => {
-  await page.goto('https://www.baidu.com/');
-  await page.locator('#kw').click();
-
-
-  await page.waitForTimeout(5_000)
-  await page.locator('#kw').fill('hehehe');
-  await page.getByRole('button', { name: '百度一下' }).click();
 });
